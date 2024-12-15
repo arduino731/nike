23b160fadb5251f2046cdec3192a6297f066c40b
arduino731 <arduino731@gmail.com>
2024-12-15 15:01:48 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 padding-b">
+      Hero 
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffer
+    </section>
+    <section className="padding">
+      CustomerReviews
+    </section>
+    <section className="padding">
+      Subscribe 
+    </section>
+    <section className="padding bg-black">
+      Footer 
+    </section>
+  </main>
+)
+
+
+
+export default App;
